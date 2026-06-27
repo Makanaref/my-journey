4d62ab643433218c470fd84fdc41a3c5269d5ede
makan <makan.aref@gmail.com>
2026-06-27 21:56:12 +0330

    Day 7 - add and subtract functions

--- calculator.py
+++ calculator.py
@@ -1,2 +1,14 @@
-print('Hello, Git')
-print('Hello, Git')
+print("Welcome to Calculator!")
+print("1. Add")
+print("2. Subtract")
+
+choice = input("Choose (1 or 2): ")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if choice == "1":
+    print("Result:", num1 + num2)
+elif choice == "2":
+    print("Result:", num1 - num2)
+else:
+    print("Invalid choice!")
