f994de65ced69e982611b74bd2f471a79286f7c5
makan <makan.aref@gmail.com>
2026-06-27 21:58:36 +0330

    Day 8 - add multiply and divide

--- calculator.py
+++ calculator.py
@@ -1,8 +1,10 @@
 print("Welcome to Calculator!")
 print("1. Add")
 print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
 
-choice = input("Choose (1 or 2): ")
+choice = input("Choose (1-4): ")
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 
@@ -10,5 +12,12 @@ if choice == "1":
     print("Result:", num1 + num2)
 elif choice == "2":
     print("Result:", num1 - num2)
+elif choice == "3":
+    print("Result:", num1 * num2)
+elif choice == "4":
+    if num2 == 0:
+        print("Error: Cannot divide by zero!")
+    else:
+        print("Result:", num1 / num2)
 else:
     print("Invalid choice!")
