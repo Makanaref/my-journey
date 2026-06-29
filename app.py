41bcca480012b70131c90434e7471a9c8b691d30
makan <makan.aref@gmail.com>
2026-06-29 23:20:47 +0330

    Day 53 - add skills page

--- app.py
+++ app.py
@@ -91,3 +91,7 @@ def blog():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
+
+@app.route("/skills")
+def skills():
+    return render_template("skills.html")
